df547fa9ac35a0264e84bfb033249d52fc0f4d4d
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-27 18:07:26 +0330

    regesteri-if-else

---
+++ js.js
@@ -0,0 +1,10 @@
+var userName = prompt("enter your user name: ..");
+var pass = prompt("enter your password:...");
+
+if (userName.length < 3 || pass.length < 8) {
+    alert("لطفا اسم بیشتر از سه کارکتر و پسورد بیشتر از 8 کارکتر باشد");
+} else {
+    alert("خوش امدید");
+   
+}
+
